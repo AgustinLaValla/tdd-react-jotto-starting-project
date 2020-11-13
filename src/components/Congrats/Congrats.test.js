a109a7a792bc498b067dbd5c5102ad4388fb1383
Agustín La Valla <agulavalla@gmail.com>
2020-11-13 00:16:42 -0300

    setupTests configured

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Congrats from './Congrats';
 import { findByTestAtrr, checkProps } from '../../../test/test.utils';
 
-configure({ adapter: new Adapter() });
 
 const defaultProps = { success: false };
 
